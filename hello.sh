dd496753175a663a0fc6cc6bdd10f28386bbdb02
Jose Germosen <josegermosen@protonmail.com>
2023-07-01 15:36:13 -0400

    ignore

--- hello.sh
+++ hello.sh
@@ -1,1 +1,10 @@
-echo "Hello World!"
+#! /bin/bash
+
+#this is for printing something on screen.
+#echo "Hello World!"
+ 
+#this add text but overide the previews one.
+#cat > hello.txt 
+
+#this is for adding text to the file.
+cat >> hello.txt
